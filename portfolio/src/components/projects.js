2b5b545de8600c0c16ecdacabda227cd6e70e58f
rahul-sarma <rahulsarma7007@gmail.com>
2020-06-08 23:48:33 +0530

    project section started

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
-import './main.css'
+import './projects.css'
 
 
 class Projects extends Component {
     render() { 
-        return ( 
-            <div style={{ width:'100%', margin:'auto' }}>
-                <h1>Projects</h1> 
+        return (
+            <React.Fragment>
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img_avatar.png" alt="Avatar" style={{width:'300px',height:'300px'}} />
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>John Doe</h1> 
+                        <p>Architect & Engineer</p> 
+                        <p>We love that guy</p>
+                    </div>
+                </div>
             </div>
+            <div class="flip-card" style={{float:'left'}}>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img_avatar.png" alt="Avatar" style={{width:'300px',height:'300px'}} />
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>John Doe</h1> 
+                        <p>Architect & Engineer</p> 
+                        <p>We love that guy</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-card" style={{float:'left'}}>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img_avatar.png" alt="Avatar" style={{width:'300px',height:'300px'}} />
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>John Doe</h1> 
+                        <p>Architect & Engineer</p> 
+                        <p>We love that guy</p>
+                    </div>
+                </div>
+            </div>
+            </React.Fragment> 
         )
     }
 }
